d0e855e83a01e59d57559a8edfb9f3084b0c85a4
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 00:31:58 +0200

    Update AppDatabase.kt

--- app/src/main/java/com/sim/card/data/AppDatabase.kt
+++ app/src/main/java/com/sim/card/data/AppDatabase.kt
@@ -3,14 +3,6 @@ package com.sim.card.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Entity(tableName = "cards")
-data class Card(
-    @PrimaryKey val id: String = ""
-)
-
-@Dao
-interface CardDao
-
 @Database(entities = [Card::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cardDao(): CardDao
