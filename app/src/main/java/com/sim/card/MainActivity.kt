76741d89442c2ab86094ebc4e4d9ee6d89834a75
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 00:33:04 +0200

    Update MainActivity.kt

--- app/src/main/java/com/sim/card/MainActivity.kt
+++ app/src/main/java/com/sim/card/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.sim.card
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.sim.card.ui.AppNav
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { AppNav() }   // we add AppNav later
+        setContent { AppNav() }
     }
 }
