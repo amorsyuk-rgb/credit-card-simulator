174b3b42316e266a72a6f45e87729fa45bde3d0e
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 01:52:30 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,22 @@
 buildscript {
+    ext.kotlin_version = '1.9.0'
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath 'com.android.tools.build:gradle:8.1.3'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
     }
 }
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register('clean', Delete) {
+    delete rootProject.buildDir
 }
