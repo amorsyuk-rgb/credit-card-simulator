03d8ac1cb370d607ad0675bda90cdac826bb1f70
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 01:30:34 +0200

    Update CardDao.kt

--- app/src/main/java/com/sim/card/data/CardDao.kt
+++ app/src/main/java/com/sim/card/data/CardDao.kt
@@ -1,6 +1,17 @@
 package com.sim.card.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
-interface CardDao
+interface CardDao {
+    @Query("SELECT * FROM cards")
+    suspend fun getAllCards(): List<Card>
+    
+    @Insert
+    suspend fun insertCard(card: Card)
+    
+    @Query("DELETE FROM cards WHERE id = :cardId")
+    suspend fun deleteCard(cardId: Int)
+}
