029f91ec881c4d2aa6420b4a12fdc17446889bfd
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-08 23:35:25 +0200

    Create Nav.kt

---
+++ app/src/main/java/com/sim/card/ui/Nav.kt
@@ -0,0 +1,14 @@
+package com.sim.card.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNav() {
+    val nav = rememberNavController()
+    NavHost(nav, startDestination = "cards") {
+        composable("cards") { /* TODO: CardsListScreen */ }
+    }
+}
