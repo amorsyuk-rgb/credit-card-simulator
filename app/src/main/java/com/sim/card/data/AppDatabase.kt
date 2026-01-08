e9c24be28628231b609bb2ef03d57d77fc3ae4f5
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 00:43:45 +0200

    Delete app/src/main/java/com/sim/card/data/AppDatabase.kt

--- app/src/main/java/com/sim/card/data/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.sim.card.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Card::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun cardDao(): CardDao
-}
