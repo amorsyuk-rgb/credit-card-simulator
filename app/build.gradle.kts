b3d643ff685c06f08ddd0a9ce44c06df4396639c
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-08 23:13:05 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,16 @@ plugins {
 android {
     namespace = "com.sim.card"
     compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.sim.card"
         minSdk = 26
@@ -15,24 +25,26 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlinOptions {
-    jvmTarget = "17"
-}
 
     buildTypes {
         named("release") {
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
-    buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.6" }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.6"
+    }
 }
+
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
