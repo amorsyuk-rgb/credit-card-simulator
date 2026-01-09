1cab728861235dd8cac0d66ea867250dbd268d51
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 03:00:48 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("kotlin-kapt") version "1.9.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
