56efbf6a1a3bdc9d55e709ade5de4fe7e9361449
amorsyuk-rgb <amorsyuk@gmail.com>
2026-01-09 03:45:18 +0200

    Update MainActivity.kt

--- app/src/main/java/com/sim/card/MainActivity.kt
+++ app/src/main/java/com/sim/card/MainActivity.kt
@@ -3,13 +3,47 @@ package com.sim.card
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.sim.card.ui.AppNav
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { AppNav() }
+        setContent {
+            AppContent()
+        }
     }
+}
+
+@Composable
+fun AppContent() {
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Credit Card Simulator")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppContent()
 }
